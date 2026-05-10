c69043898223531fb576a3e732aae5ee0ba3021a
cart8910 <carteroverethernet@gmail.com>
2026-05-10 11:32:46 -0700

    added background

--- game.py
+++ game.py
@@ -1,7 +1,12 @@
+#Libs
 import pygame
 from pygame.locals import *
 from pygame.math import Vector2
+import os
+
+#Local files
 from constants import *
+from pacman import Pacman
 
 class MainGame(object):
     def __init__(self):
@@ -9,18 +14,26 @@ class MainGame(object):
         self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
         self.background = None
 
+        #this ensures we load from the Assets folder even if the project is run from another working dir.
+        image_path = os.path.join(os.path.dirname(__file__), "Assets", "maze.bmp")
+
+        self.bg_image = pygame.image.load(image_path)
+        self.bg_image = pygame.transform.scale(self.bg_image, SCREENSIZE)
+
         self.clock = pygame.time.Clock()
 
     def setBackground(self):
-        self.background = pygame.surface.Surface(SCREENSIZE).convert()
-        self.background.fill(BLACK)
+        self.background = self.bg_image
 
     def startGame(self):
         self.setBackground()
+        self.pacman = Pacman()
 
     def update(self):
         delta = self.clock.tick(30) / 1000.0 #time since last frame in seconds
         
+        self.pacman.update(delta)
+
         self.checkEvents()
         self.render()
 
@@ -31,6 +44,9 @@ class MainGame(object):
                 exit()
 
     def render(self):
+        self.screen.blit(self.background, (0, 0))
+        self.pacman.render(self.screen)
+        
         pygame.display.update()
 
 
